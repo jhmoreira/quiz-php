64447e194fafbbc74b2cbdbd1201d76cda323803
João Moreira <joaomoreirati98@gmail.com>
2025-11-18 10:05:10 -0300

    Inicializando projeto

---
+++ public/index.php
@@ -0,0 +1,20 @@
+<?php
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'quiz');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
+
+$options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES => false,
+];
+
+try{
+    $pdo = new PDO("mysql:host=". DB_HOST. ";charset= utf8mb4", DB_USER, DB_PASSWORD,$options);
+}catch(PDOException $e){
+    $pdo = NULL;
+}
+
+
+?>
